597aa0f5af73468268c5c6992e75c875a8afeddd
christinezosche <christine.zosche@gmail.com>
2020-11-18 09:00:37 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(array, string) {
+   
+    let matches = array.filter(item => {
+        return item.toUpperCase() === string.toUpperCase();
+      });
+    return matches
+}
+
+function fuzzyMatch(array, string) {
+    let num = string.length
+    let matches = array.filter(item => {
+        let letters = item.substring(0, num)
+        return letters.toUpperCase() === string.toUpperCase();
+      });
+    return matches
+
+}
+
+function matchName(array, string) {
+    let matches = array.filter(item => {
+        return item.name === string;
+      });
+    return matches
+}
